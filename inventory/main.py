a044c5ccbd12c04bee4e7b08208a7f61c3cfad12
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-21 15:41:43 +0530

    Bug

--- inventory/main.py
+++ inventory/main.py
@@ -7,9 +7,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
-    allow_methods=["*" ]
+    allow_methods=["*" ],
     allow_headers=["*"],
 )
+
 
 redis = get_redis_connection(
     host="redis-16337.c301.ap-south-1-1.ec2.cloud.redislabs.com",
@@ -29,4 +30,8 @@ class Product(HashModel):
 
 @app.get("/products")
 def all():
-    return Product.all_pks(); 
+    return Product.all_pks()
+
+@app.post("/products")
+def create(product: Product):
+    return product.save()
