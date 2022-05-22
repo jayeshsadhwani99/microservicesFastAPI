08b5484246f25b17dd33f14a4137fe2e96438e3b
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-22 15:16:38 +0530

    Send all prodcuts

--- inventory/main.py
+++ inventory/main.py
@@ -1,3 +1,4 @@
+from itertools import product
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
@@ -30,7 +31,17 @@ class Product(HashModel):
 
 @app.get("/products")
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
 
 @app.post("/products")
 def create(product: Product):
