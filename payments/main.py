cd31cb6bf7d80edf318ee41941e81c9920023d0a
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-22 15:39:27 +0530

    Create basic order

--- payments/main.py
+++ payments/main.py
@@ -1,6 +1,9 @@
+from turtle import rt
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests
 
 app = FastAPI()
 
@@ -18,3 +21,40 @@ redis = get_redis_connection(
     password="KU17wk6VZw2c0gwekQDdJLLBh3uI0Tna",
     decode_responses = True,
 )
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str # pending, confirmed, refunded
+
+    class Meta:
+        database = redis
+
+@app.post("/orders")
+async def create(request: Request): # id and the quantity
+    body = await request.json()
+
+    req = requests.get("http://localhost:8000/products/%s" % body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id = body['id'],
+        price = product['price'],
+        fee = 0.2 * product['price'],
+        total = 1.2 * product['price'],
+        quantity = body['quantity'],
+        status = 'pending'
+    )
+
+    order.save()
+
+    order_completed(order)
+
+    return order
+
+def order_completed(order: Order):
+    order.status = "completed";
+    order.save()
